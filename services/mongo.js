9907c619edeb8aa328544b32dccc98aa00b1bf85
Attitude <developer_03@wrfront.com>
2026-04-11 19:32:54 +0700

    Fix connectMongoDB

--- services/mongo.js
+++ services/mongo.js
@@ -1,30 +1,13 @@
 const mongoose = require('mongoose')
 
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
 async function connectMongoDB() {
-  if (cached.conn) {
-    return cached.conn
+  try {
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message)
+    process.exit(1)
   }
-
-  if (!process.env.MONGODB_URI) {
-    throw new Error('MONGODB_URI is not set')
-  }
-
-  if (!cached.promise) {
-    mongoose.set('bufferCommands', false)
-
-    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000
-    }).then((mongooseInstance) => mongooseInstance)
-  }
-
-  cached.conn = await cached.promise
-  return cached.conn
 }
 
 module.exports = connectMongoDB
